8b37d971f3f6f281a5ee1c7b982ce3d5f23b9839
Roman E <livcey@gmail.com>
2021-02-09 11:35:13 +0300

    Minor changes

--- Get-BingDaily.ps1
+++ Get-BingDaily.ps1
@@ -47,27 +47,28 @@ $Locales = @(
 foreach ($locale in $Locales) {
     Write-Host Processing [$locale] -ForegroundColor Yellow
     [string]$uri = "$hostname/HPImageArchive.aspx?format=xml&idx=0&n=$maxItemCount&mkt=$locale"
-    $request = Invoke-WebRequest -Uri $uri
+   $request = Invoke-WebRequest -Uri $uri
     [xml]$content = $request.Content
     $items = New-Object System.Collections.ArrayList
     foreach ($xmlImage in $content.images.image) {
         [datetime]$imageDate = [datetime]::ParseExact($xmlImage.startdate, 'yyyyMMdd', $null)
         [string]$imageUrl = "$hostname$($xmlImage.urlBase)_$resolution.jpg"
+        [string]$imageUrlBase = $xmlImage.urlBase
         # Add item to our array list
         $item = New-Object System.Object
         $item | Add-Member -Type NoteProperty -Name date -Value $imageDate
         $item | Add-Member -Type NoteProperty -Name url -Value $imageUrl
+        $item | Add-Member -Type NoteProperty -Name imageUrlBase -Value $imageUrlBase
         $null = $items.Add($item)
     }
     Write-Host "Downloading:"
     $client = New-Object System.Net.WebClient
     foreach ($item in $items) {
         $baseName = $item.date.ToString("yyyy-MM-dd")
-        $destination = "$downloadFolder\$baseName.jpg"
-        $urlBase = $xmlImage.urlBase -replace "\/th\?id=",""
+        $urlBase = $item.imageUrlBase -replace "\/th\?id=",""
         $url = $item.url
-        #$destination = "$downloadFolder\Bing Daily $baseName $Locale $urlBase.jpg"
-        $destination = "D:\Bing Daily $baseName $Locale $urlBase.jpg"
+        $destination = "$downloadFolder\Bing Daily $baseName $Locale $urlBase.jpg"
+        #$destination = "D:\Bing Daily $baseName $Locale $urlBase.jpg"
         Write-Host $basename : $url
         # Download the enclosure if we haven't done so already
         if (!(Test-Path $destination)) {
