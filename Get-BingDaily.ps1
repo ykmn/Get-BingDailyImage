940079facd5045b3d5dda94fdfc7ec983aa7eaa2
Roman E <livcey@gmail.com>
2021-02-09 15:04:13 +0300

    Download unique images only

--- Get-BingDaily.ps1
+++ Get-BingDaily.ps1
@@ -3,7 +3,9 @@
     Download latest Bing Daily Images for different locales
 
 .DESCRIPTION
-    Download latest Bing Daily Images for different locales
+    Download latest Bing Daily Images for different locales in maximum resolution:
+    if image is not exist in top resolution we're saving previous resolution.
+    Images are unique i.e. if the same image exists in different locales we're saving only one.
 
 .LINK
     https://github.com/ykmn/Get-BingDaily
@@ -16,6 +18,7 @@
 v1.00 2021-02-09 Initial release
 #>
 
+$ErrorActionPreference = 'SilentlyContinue'
 # Check if download folder exists and otherwise create it
 [string]$downloadFolder = "$([Environment]::GetFolderPath("MyPictures"))\Bing Wallpapers"
 #[string]$downloadFolder = $PSScriptRoot
@@ -24,56 +27,74 @@ if (!(Test-Path $downloadFolder)) {
 }
 
 $Locales = @(
-# available locales are:
+# Available locales are:
 #    'ar-XA', 'bg-BG', 'cs-CZ', 'da-DK', 'de-AT', 'de-CH', 'de-DE', 'el-GR', 'en-AU', 'en-CA',
 #    'en-GB', 'en-ID', 'en-IE', 'en-IN', 'en-MY', 'en-NZ', 'en-PH', 'en-SG', 'en-US', 'en-XA',
 #    'en-ZA', 'es-AR', 'es-CL', 'es-ES', 'es-MX', 'es-US', 'es-XL', 'et-EE', 'fi-FI', 'fr-BE',
 #    'fr-CA', 'fr-CH', 'fr-FR', 'he-IL', 'hr-HR', 'hu-HU', 'it-IT', 'ja-JP', 'ko-KR', 'lt-LT',
 #    'lv-LV', 'nb-NO', 'nl-BE', 'nl-NL', 'pl-PL', 'pt-BR', 'pt-PT', 'ro-RO', 'ru-RU', 'sk-SK',
 #    'sl-SL', 'sv-SE', 'th-TH', 'tr-TR', 'uk-UA', 'zh-CN', 'zh-HK', 'zh-TW'
+# Currently only the values 'de-DE', 'en-AU', 'en-CA', 'en-GB', 'en-IN', 'en-US', 'fr-CA', 'fr-FR', 'ja-JP', 'zh-CN'
+# will have their own localized version. Other values will be considered as the "Rest of the World" by Bing.
 
-    'de-CH', 'de-DE', 'en-AU', 'en-IN', 'en-GB', 'en-US', 'fr-CA', 'fr-FR',  'ja-JP', 'zh-CN'
+    'de-DE', 'en-AU', 'en-CA', 'en-GB', 'en-IN', 'en-US', 'fr-CA', 'fr-FR', 'ja-JP', 'zh-CN'
 );
 
 [string]$hostname = "https://www.bing.com"
 # Download the latest $files wallpapers
-[int]$files = 3
+[int]$files = 1
 # Max item count: the number of images we'll query for
 [int]$maxItemCount = [System.Math]::max(1, [System.Math]::max($files, 8))
 # Available resolutions of the image to download:
 # '1024x768', '1280x720', '1366x768', '1920x1080', '1920x1200'
-[string]$resolution = '1920x1080'
+$resolutions = @( '1920x1080', '1920x1200' )
+
+Write-Host "Processing: " -NoNewline -ForegroundColor Yellow
+$items = New-Object System.Collections.ArrayList
 
 foreach ($locale in $Locales) {
-    Write-Host Processing [$locale] -ForegroundColor Yellow
+    Write-Host [$locale"] " -NoNewline
     [string]$uri = "$hostname/HPImageArchive.aspx?format=xml&idx=0&n=$maxItemCount&mkt=$locale"
-   $request = Invoke-WebRequest -Uri $uri
+    $request = Invoke-WebRequest -Uri $uri
     [xml]$content = $request.Content
-    $items = New-Object System.Collections.ArrayList
+
     foreach ($xmlImage in $content.images.image) {
-        [datetime]$imageDate = [datetime]::ParseExact($xmlImage.startdate, 'yyyyMMdd', $null)
-        [string]$imageUrl = "$hostname$($xmlImage.urlBase)_$resolution.jpg"
-        [string]$imageUrlBase = $xmlImage.urlBase
-        # Add item to our array list
-        $item = New-Object System.Object
-        $item | Add-Member -Type NoteProperty -Name date -Value $imageDate
-        $item | Add-Member -Type NoteProperty -Name url -Value $imageUrl
-        $item | Add-Member -Type NoteProperty -Name imageUrlBase -Value $imageUrlBase
-        $null = $items.Add($item)
+        foreach ($resolution in $resolutions) {
+            [datetime]$imageDate = [datetime]::ParseExact($xmlImage.startdate, 'yyyyMMdd', $null)
+            [string]$imageUrl = "$hostname$($xmlImage.urlBase)_$resolution.jpg"
+            [string]$imageUrlBase = $xmlImage.urlBase
+            # Add item to our array list
+            $item = New-Object System.Object
+            $item | Add-Member -Type NoteProperty -Name date -Value $imageDate
+            $item | Add-Member -Type NoteProperty -Name url -Value $imageUrl
+            $imageUrlBase = $imageUrlBase -replace "\/th\?id=",""
+            $imageUrlBase = $imageUrlBase -replace $locale,""
+            $imageUrlBase = $imageUrlBase -replace "OHR.",""
+            $imageUrlBase = $imageUrlBase -replace "_\d{8,12}.",""
+            $item | Add-Member -Type NoteProperty -Name imageUrlBase -Value $imageUrlBase
+            $null = $items.Add($item)
+        }
     }
-    Write-Host "Downloading:"
-    $client = New-Object System.Net.WebClient
-    foreach ($item in $items) {
-        $baseName = $item.date.ToString("yyyy-MM-dd")
-        $urlBase = $item.imageUrlBase -replace "\/th\?id=",""
-        $url = $item.url
-        $destination = "$downloadFolder\Bing Daily $baseName $Locale $urlBase.jpg"
-        #$destination = "D:\Bing Daily $baseName $Locale $urlBase.jpg"
-        Write-Host $basename : $url
-        # Download the enclosure if we haven't done so already
-        if (!(Test-Path $destination)) {
-            Write-Debug "Downloading image to $destination"
-        $client.DownloadFile($url, "$destination")
-       }
+}
+
+$items = $($items | Sort-Object -Unique -Property imageUrlBase)
+<#
+Write-Host "`nFound unique images:" -ForegroundColor Yellow
+$items | Format-Table
+#>
+
+Write-Host "Downloading:"
+$client = New-Object System.Net.WebClient
+foreach ($item in $items) {
+    $baseDate = $item.date.ToString("yyyy-MM-dd")
+    $baseName = $item.imageUrlBase
+    $url = $item.url
+    $destination = "$downloadFolder\Bing Daily $baseDate $baseName.jpg"
+    #$destination = "D:\Bing Daily $baseDate $Locale $baseName.jpg"
+    Write-Host $baseDate : $url
+    # Download the enclosure if we haven't done so already
+    if (!(Test-Path $destination)) {
+        Write-Debug "Downloading image to $destination"
+    $client.DownloadFile($url, "$destination")
     }
 }
