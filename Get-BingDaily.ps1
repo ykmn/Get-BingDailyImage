46d68e7dfde5636e86063e80e43a7261c204dab2
Roman E <livcey@gmail.com>
2021-02-11 16:45:25 +0300

    Update Get-BingDaily.ps1

--- Get-BingDaily.ps1
+++ Get-BingDaily.ps1
@@ -16,6 +16,7 @@
 #>
 
 <#
+Roman Ermakov <r.ermakov@emg.fm>
 v1.00 2021-02-09 Initial release
 #>
 
@@ -28,17 +29,18 @@ if (!(Test-Path $downloadFolder)) {
 }
 
 $Locales = @(
-# Available locales are:
-    'ar-XA', 'bg-BG', 'cs-CZ', 'da-DK', 'de-AT', 'de-CH', 'de-DE', 'el-GR', 'en-AU', 'en-CA',
-    'en-GB', 'en-ID', 'en-IE', 'en-IN', 'en-MY', 'en-NZ', 'en-PH', 'en-SG', 'en-XA',
-    'en-ZA', 'es-AR', 'es-CL', 'es-ES', 'es-MX', 'es-US', 'es-XL', 'et-EE', 'fi-FI', 'fr-BE',
-    'fr-CA', 'fr-CH', 'fr-FR', 'he-IL', 'hr-HR', 'hu-HU', 'it-IT', 'ja-JP', 'ko-KR', 'lt-LT',
-    'lv-LV', 'nb-NO', 'nl-BE', 'nl-NL', 'pl-PL', 'pt-BR', 'pt-PT', 'ro-RO', 'ru-RU', 'sk-SK',
-    'sl-SL', 'sv-SE', 'th-TH', 'tr-TR', 'uk-UA', 'zh-CN', 'zh-HK', 'zh-TW', 'en-US' 
+<# Available locales are:
+#    'ar-XA', 'bg-BG', 'cs-CZ', 'da-DK', 'de-AT', 'de-CH', 'de-DE', 'el-GR', 'en-AU', 'en-CA',
+#    'en-GB', 'en-ID', 'en-IE', 'en-IN', 'en-MY', 'en-NZ', 'en-PH', 'en-SG', 'en-XA',
+#    'en-ZA', 'es-AR', 'es-CL', 'es-ES', 'es-MX', 'es-US', 'es-XL', 'et-EE', 'fi-FI', 'fr-BE',
+#    'fr-CA', 'fr-CH', 'fr-FR', 'he-IL', 'hr-HR', 'hu-HU', 'it-IT', 'ja-JP', 'ko-KR', 'lt-LT',
+#    'lv-LV', 'nb-NO', 'nl-BE', 'nl-NL', 'pl-PL', 'pt-BR', 'pt-PT', 'ro-RO', 'ru-RU', 'sk-SK',
+#    'sl-SL', 'sv-SE', 'th-TH', 'tr-TR', 'uk-UA', 'zh-CN', 'zh-HK', 'zh-TW', 'en-US' 
 # Currently only the values 'de-DE', 'en-AU', 'en-CA', 'en-GB', 'en-IN', 'en-US', 'fr-CA', 'fr-FR', 'ja-JP', 'zh-CN'
 # will have their own localized version. Other values will be considered as the "Rest of the World" by Bing.
+#>
 
-#    'de-DE', 'en-AU', 'en-CA', 'en-GB', 'en-IN', 'en-US', 'fr-CA', 'fr-FR', 'ja-JP', 'zh-CN'
+    'de-DE', 'en-AU', 'en-CA', 'en-GB', 'en-IN', 'en-US', 'fr-CA', 'fr-FR', 'ja-JP', 'zh-CN'
 );
 
 [string]$hostname = "https://www.bing.com"
@@ -48,6 +50,7 @@ $Locales = @(
 [int]$maxItemCount = [System.Math]::max(1, [System.Math]::max($files, 8))
 # Available resolutions of the image to download:
 # '1024x768', '1280x720', '1366x768', '1920x1080', '1920x1200'
+$maxItemCount
 $resolutions = @( '1920x1080', '1920x1200' )
 
 Write-Host "Processing: " -NoNewline -ForegroundColor Yellow
