d9c2a73a204cbf97574523855c691fde094dac1f
Roman E <livcey@gmail.com>
2021-02-26 18:55:19 +0300

    Update Get-BingDaily.ps1

--- Get-BingDaily.ps1
+++ Get-BingDaily.ps1
@@ -17,6 +17,7 @@
 
 <#
 Roman Ermakov <r.ermakov@emg.fm>
+v1.01 2021-02-26 Compare online images list and already downloaded files
 v1.00 2021-02-09 Initial release
 #>
 
@@ -30,14 +31,14 @@ if (!(Test-Path $downloadFolder)) {
 
 $Locales = @(
 <# Available locales are:
-#    'ar-XA', 'bg-BG', 'cs-CZ', 'da-DK', 'de-AT', 'de-CH', 'de-DE', 'el-GR', 'en-AU', 'en-CA',
-#    'en-GB', 'en-ID', 'en-IE', 'en-IN', 'en-MY', 'en-NZ', 'en-PH', 'en-SG', 'en-XA',
-#    'en-ZA', 'es-AR', 'es-CL', 'es-ES', 'es-MX', 'es-US', 'es-XL', 'et-EE', 'fi-FI', 'fr-BE',
-#    'fr-CA', 'fr-CH', 'fr-FR', 'he-IL', 'hr-HR', 'hu-HU', 'it-IT', 'ja-JP', 'ko-KR', 'lt-LT',
-#    'lv-LV', 'nb-NO', 'nl-BE', 'nl-NL', 'pl-PL', 'pt-BR', 'pt-PT', 'ro-RO', 'ru-RU', 'sk-SK',
-#    'sl-SL', 'sv-SE', 'th-TH', 'tr-TR', 'uk-UA', 'zh-CN', 'zh-HK', 'zh-TW', 'en-US' 
-# Currently only the values 'de-DE', 'en-AU', 'en-CA', 'en-GB', 'en-IN', 'en-US', 'fr-CA', 'fr-FR', 'ja-JP', 'zh-CN'
-# will have their own localized version. Other values will be considered as the "Rest of the World" by Bing.
+    'ar-XA', 'bg-BG', 'cs-CZ', 'da-DK', 'de-AT', 'de-CH', 'de-DE', 'el-GR', 'en-AU', 'en-CA',
+    'en-GB', 'en-ID', 'en-IE', 'en-IN', 'en-MY', 'en-NZ', 'en-PH', 'en-SG', 'en-XA',
+    'en-ZA', 'es-AR', 'es-CL', 'es-ES', 'es-MX', 'es-US', 'es-XL', 'et-EE', 'fi-FI', 'fr-BE',
+    'fr-CA', 'fr-CH', 'fr-FR', 'he-IL', 'hr-HR', 'hu-HU', 'it-IT', 'ja-JP', 'ko-KR', 'lt-LT',
+    'lv-LV', 'nb-NO', 'nl-BE', 'nl-NL', 'pl-PL', 'pt-BR', 'pt-PT', 'ro-RO', 'ru-RU', 'sk-SK',
+    'sl-SL', 'sv-SE', 'th-TH', 'tr-TR', 'uk-UA', 'zh-CN', 'zh-HK', 'zh-TW', 'en-US' 
+Currently only the values 'de-DE', 'en-AU', 'en-CA', 'en-GB', 'en-IN', 'en-US', 'fr-CA', 'fr-FR', 'ja-JP', 'zh-CN'
+will have their own localized version. Other values will be considered as the "Rest of the World" by Bing.
 #>
     'de-DE', 'en-AU', 'en-CA', 'en-GB', 'en-IN', 'en-US', 'fr-CA', 'fr-FR', 'ja-JP', 'zh-CN'
 );
@@ -47,7 +48,6 @@ $resolutions = @( '1920x1080', '1920x1200' )
 
 Write-Host "Processing locales: " -NoNewline -ForegroundColor Yellow
 $items = New-Object System.Collections.ArrayList
-
 foreach ($locale in $Locales) {
     Write-Host [$locale"] " -NoNewline
     [string]$uri = "$hostname/HPImageArchive.aspx?format=xml&idx=0&n=8&mkt=$locale"
@@ -68,32 +68,48 @@ foreach ($locale in $Locales) {
             $imageUrlBase = $imageUrlBase -replace "OHR.",""
             $imageUrlBase = $imageUrlBase -replace "ROW",""
             $imageUrlBase = $imageUrlBase -replace "_\d{8,12}.",""
-            $item | Add-Member -Type NoteProperty -Name imageUrlBase -Value $imageUrlBase
+            $item | Add-Member -Type NoteProperty -Name id -Value $imageUrlBase
             $null = $items.Add($item)
         }
     }
 }
-
-$items = $($items | Sort-Object -Unique -Property imageUrlBase)
-<#
-Write-Host "`nUnique images:" -ForegroundColor Yellow
+$items = $($items | Sort-Object -Unique -Property id)
+Write-Host "`nUnique images found in all locales:" -ForegroundColor Yellow
 $items | Format-Table
+
+$files = New-Object System.Collections.ArrayList
+foreach ($jpgFile in (Get-ChildItem -Path $downloadFolder)) {
+    [string]$id = $jpgFile.Name -replace "Bing\ Daily\ \d{4}\-\d{2}\-\d{2}\ ",""
+    [string]$id = $id -replace "\.jpg",""
+    $file = New-Object System.Object
+    $file | Add-Member -Type NoteProperty -Name path -Value $jpgFile.FullName
+    $file | Add-Member -Type NoteProperty -Name id -Value $id
+    $null = $files.Add($file)
+}
+$files | Sort-Object -Unique -Property id
+<#
+Write-Host "`nUnique existing files:" -ForegroundColor Yellow
+$files | Format-Table
+#>
+$c = Compare-Object -ReferenceObject $items -DifferenceObject $files -Property id -PassThru 
+<#
+Write-Host "`nComparison:" -ForegroundColor Yellow
+$c | Format-Table
 #>
 
 Write-Host "`nDownloading:" -ForegroundColor Yellow
 $client = New-Object System.Net.WebClient
-foreach ($item in $items) {
-    $baseDate = $item.date.ToString("yyyy-MM-dd")
-    $baseName = $item.imageUrlBase
-    $url = $item.url
-    $destination = Join-Path -Path "$downloadFolder" -ChildPath "Bing Daily $baseDate $baseName.jpg"
-    Write-Host $baseDate : $url
-    # Download the enclosure if we haven't done so already
-    if (!(Test-Path $destination)) {
-        Write-Debug "Downloading image to $destination"
-    $client.DownloadFile($url, "$destination")
+foreach ($cc in $c)  {
+    if ($cc.SideIndicator -eq "<=") {
+        $baseDate = $cc.url.ToString("yyyy-MM-dd")
+        $baseName = $cc.id
+        $url = $cc.url
+        $destination = Join-Path -Path "$downloadFolder" -ChildPath "Bing Daily $baseDate $baseName.jpg"
+        Write-Host $baseDate : $url with BASENAME $baseName to $destination
+        #Write-Host "Downloading image to $destination"
+        $client.DownloadFile($url, "$destination")
     }
 }
-
 Write-Host "`nAll done."
 Start-Sleep -Seconds 5
+break
